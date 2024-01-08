46020b98521c58f1d05221acc97a12490f0e748e
Océane Princep <94532496+oce-prcp@users.noreply.github.com>
2024-01-08 17:50:00 +0100

    fix

--- routes/databaseRoute.js
+++ routes/databaseRoute.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const databaseController = require('../controller/databaseController')
 const middleware = require('../middleware/middleware')
 
-router.get('/createAllTable', databaseController.createAllTable)
+router.get('/createAllTable', middleware.checkAdmin, databaseController.createAllTable)
 
 module.exports = router
